7d5a115091c1b939b877b706e442f1842e0772cd
Lucas Alcantara Rodrigues Volpati <lcs.rodrigues16@hotmail.com>
2021-08-08 12:47:51 -0300

    Criação de novos métodos de select.
    Criação de:
    getList();
    search();
    dados();
    
    Adicionado a constante de data.

--- DAO/classes/Usuario.php
+++ DAO/classes/Usuario.php
@@ -60,6 +60,52 @@ class Usuario {
 			$this->setDtCadastro(new DateTime($row['dtcadastro']));
 		}
 	}
+
+
+	/*** Método que carrega uma lista dos usuarios cadastrados e ordena pelo nome 'login'. ***/
+	public static function getList() {
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+	}
+
+	/*** Método que carrega os usuarios que contem as letras informadas usando comando SQL 'LIKE'.  ***/
+
+	public static function search($login) {
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(':SEARCH'=>"%" . $login . "%"));
+	} 
+
+	/*** Método que recupera os dados do usuario desde que sejam informados corretamente o login e senha ***/
+
+	public function dados($login, $pass) {
+
+		$sql = new Sql();
+
+		$result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+
+			":LOGIN"=>$login,
+			":PASSWORD"=>$pass
+		));
+
+		if (isset($result)) {
+
+			$row = $result[0];
+
+			$this->setIdUsuario($row['idusuario']);
+			$this->setLogin($row['deslogin']);
+			$this->setSenha($row['dessenha']);
+			$this->setDtCadastro(new DateTime($row['dtcadastro']));
+		}else {
+
+			throw new Exception("Login ou senha inválido.");
+			
+		}
+	}
+
 	//Cria um Json das informações buscadas no banco de dados.
 	public function __toString(){
 
@@ -68,7 +114,7 @@ class Usuario {
 			"idusuario"=>$this->getIdUsuario(),
 			"deslogin"=>$this->getLogin(),
 			"dessenha"=>$this->getSenha(),
-			"dtcadastro"=>$this->getDtCadastro()->format("d/m/Y")
+			"dtcadastro"=>$this->getDtCadastro()->format(DATE_COOKIE) //constante predefinida de data. Ver documentação.
 
 		));
 	}
