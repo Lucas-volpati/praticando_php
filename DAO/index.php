b87779bc1952680585df902a2d452b409be00959
Lucas Alcantara Rodrigues Volpati <lcs.rodrigues16@hotmail.com>
2021-08-08 12:50:14 -0300

    Testando os novos métodos da class Usuario
    Testando os métodos:
    getList();
    dados();
    search();

--- DAO/index.php
+++ DAO/index.php
@@ -2,12 +2,37 @@
 
 require_once ("Config.php");
 
-echo "<h1>Praticando conexão com banco de dados!</h1>";
+/* Praticando conexão com banco de dados! */
 
 
 
-$usu1 = new Usuario();
+/* ESTE COMANDO RECUPERA UM USUÁRIO APENAS */
+//$usu1 = new Usuario();
+//$usu1->loadById(4);
 
-$usu1->loadById(4);
+//echo $usu1;
 
-echo $usu1;
+
+
+/* ESTE COMANDO RECUPERA UMA LISTA DE USUARIOS */
+
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+
+
+/* ESTE COMANDO RECUPERA OS USUARIOS BUSCANDO POR PEQUENAS PALAVRAS CONTIDAS NO DESLOGIN USANDO COMANDO LIKE DO SQL */
+
+//$search = Usuario::search("an");
+
+//echo json_encode($search);
+
+
+/* CARREGA UM USUARIO DESDE QUE LOGIN E SENAH ESTEJAM CORRETOS */
+
+$usuario = new Usuario();
+
+$usuario->dados("Fox", "@#@#$");
+
+echo ($usuario);
